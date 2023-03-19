1b3ed3173d62fb8d3b020682f213a435d017591d
richardgan36 <107867775+richardgan36@users.noreply.github.com>
2023-03-19 19:14:02 +1100

    Add files via upload

---
+++ dogsVcats.py
@@ -0,0 +1,103 @@
+import tensorflow as tf
+import numpy as np
+import glob
+
+from keras import layers, Input
+from keras.models import Sequential
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+from classify_digits_CNN_funcs import plot_graph
+from generator_class import DataGenerator
+
+NUM_IMAGES = 25000
+
+# Hyperparameters
+filters_conv0 = 64
+filters_conv1 = 32
+filters_conv2 = 32
+dropout = 0.5
+n_dense0 = 32
+validation_split = 0.2
+epochs = 30
+
+class_params = {'dim': (50, 50),
+                'batch_size': 64,
+                'n_classes': 2,
+                'n_channels': 1,
+                'shuffle': True,
+                'directory': '/Users/richardgan/Pictures/Machine Learning/train_dogVcats/'}
+
+
+def main():
+    partition = {'train': [], 'validation': []}
+    labels = {}
+
+    training_samples = np.ceil((1 - validation_split) * NUM_IMAGES)
+    for i, filename in enumerate(glob.iglob('/Users/richardgan/Pictures/Machine Learning/train_dogVcats/*')):
+        ID = filename[59:]  # Name of image without directory name
+        if i < training_samples:
+            partition['train'].append(ID)
+        else:
+            partition['validation'].append(ID)
+
+        labels[ID] = 1 if ID.startswith('dog') else 0
+
+    # Generators
+    training_generator = DataGenerator(partition['train'], labels, **class_params)
+    validation_generator = DataGenerator(partition['validation'], labels, **class_params)
+
+    model = Sequential()
+    model.add(Input(shape=(*class_params['dim'], class_params['n_channels'])))
+    model.add(layers.Conv2D(filters_conv0, 5, activation='relu'))
+    # model.add(layers.Conv2D(filters_conv0, 3, activation='relu'))
+    model.add(layers.MaxPool2D())
+    model.add(layers.Dropout(dropout))
+
+    model.add(layers.Conv2D(filters_conv1, 5, activation='relu'))
+    # model.add(layers.Conv2D(filters_conv1, 3, activation='relu'))
+    model.add(layers.MaxPool2D())
+    model.add(layers.Dropout(dropout))
+
+    # model.add(layers.Conv2D(filters_conv2, 3, activation='relu'))
+    # model.add(layers.MaxPool2D())
+    # model.add(layers.Dropout(dropout))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(n_dense0, activation='relu'))
+    model.add(layers.Dropout(dropout))
+
+    model.add(layers.Dense(2, activation='sigmoid'))  # Binary output layer
+
+    # model.summary()
+
+    model.compile(optimizer=tf.keras.optimizers.Adam(),
+                  loss=tf.keras.losses.BinaryCrossentropy(),
+                  metrics=['accuracy'])
+
+    early_stopping = EarlyStopping(monitor='val_loss',
+                                   patience=3,
+                                   verbose=1)
+
+    model_checkpoint = ModelCheckpoint(filepath='trained_models/dogsVcats_model4.h5',
+                                       monitor='val_loss',
+                                       verbose=1,
+                                       save_best_only=True)
+
+    history = model.fit(x=training_generator,
+                        validation_data=validation_generator,
+                        epochs=epochs,
+                        callbacks=[early_stopping, model_checkpoint],
+                        verbose=1,
+                        use_multiprocessing=True,
+                        workers=6)
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epoch_range = np.arange(len(acc))
+    plot_graph(epoch_range, acc, val_acc, loss, val_loss)
+
+
+if __name__ == "__main__":
+    main()
---
+++ generator_class.py
@@ -0,0 +1,74 @@
+"""
+Adapted from:
+A. Amidi, S. Amidi, "A detailed example of how to use data generators with Keras", n.d.,
+available: https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
+Date of retrieval: 26 Jan 2023
+"""
+
+
+import numpy as np
+import keras
+
+from PIL import Image
+
+
+# Generates data for keras
+class DataGenerator(keras.utils.Sequence):
+    def __init__(self, list_IDs, labels, batch_size, dim, n_channels=1, n_classes=2, shuffle=True, directory=None):
+        self.dim = dim
+        self.n_channels = n_channels
+        self.batch_size = batch_size
+        self.list_IDs = list_IDs
+        self.labels = labels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.directory = directory
+        self.on_epoch_end()
+
+    def __len__(self):
+        # Number of batches per epoch
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
+
+    def __getitem__(self, index):
+        # Generate one batch of data using list index syntax
+        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
+
+        # Find list of IDs
+        list_IDs_batch = [self.list_IDs[k] for k in indexes]
+
+        # Generate data
+        X, y = self.__data_generation(list_IDs_batch)
+
+        return X, y
+
+    def on_epoch_end(self):
+        # Shuffle indexes after each epoch
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_IDs_batch):
+        # Generates data containing batch_size samples
+        X = np.empty(shape=(self.batch_size, *self.dim, self.n_channels))
+        y = np.empty(shape=(self.batch_size), dtype=int)
+
+        # Generate data
+        for i, ID in enumerate(list_IDs_batch):
+            img = np.asarray(Image.open(self.directory + ID).convert('L').resize(self.dim)) / 255
+            X[i] = np.reshape(img, (*self.dim, 1))
+
+            y[i] = self.labels[ID]
+
+        return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
+
+
+def pad_image(img_original, padded_height, padded_width):
+    img_height, img_width = img_original.shape
+    height_to_pad, width_to_pad = padded_height - img_height, padded_width - img_width
+    # Pad image with black borders on the right and bottom
+    img_width_padded = np.hstack(tup=(img_original, np.zeros(shape=(img_height, width_to_pad))))
+    img_width_and_height_padded = np.vstack(tup=(img_width_padded, np.zeros(shape=(height_to_pad, padded_width))))
+    img_width_and_height_padded = np.reshape(img_width_and_height_padded, (padded_height, padded_width, 1))
+
+    return img_width_and_height_padded
+
