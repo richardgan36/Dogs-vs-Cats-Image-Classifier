f45c9061507da3b7ec7c77ce005b7a3d8f792af7
richardgan36 <107867775+richardgan36@users.noreply.github.com>
2023-03-19 19:28:23 +1100

    Add files via upload

---
+++ dogsVcats_predict.py
@@ -0,0 +1,17 @@
+import numpy as np
+from keras import models
+from PIL import Image
+
+model = models.load_model('trained_models/dogsVcats_model3.h5')
+test_img = Image.open('/Users/richardgan/Pictures/Machine Learning/test_dogsVcats/318.jpg').convert('L')
+test_img = test_img.resize((100, 100))
+test_img = np.asarray(test_img) / 255
+test_img = np.reshape(test_img, (1, 100, 100, 1))
+
+prediction = model.predict(test_img)
+
+if np.argmax(prediction) == 1:
+    print("Image is a dog")
+
+else:
+    print("Image is a cat")
