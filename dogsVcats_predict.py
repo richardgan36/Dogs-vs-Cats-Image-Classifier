892fb2b70594e13ddd433bf0a374dd50ebb68e65
richardgan36 <107867775+richardgan36@users.noreply.github.com>
2023-03-21 10:05:50 +1100

    Update dogsVcats_predict.py

--- dogsVcats_predict.py
+++ dogsVcats_predict.py
@@ -1,9 +1,13 @@
+"""
+This script loads a trained dogs vs. cats model and predicts whether an image contains a cat or a dog.
+"""
+
 import numpy as np
 from keras import models
 from PIL import Image
 
-model = models.load_model('trained_models/dogsVcats_model3.h5')
-test_img = Image.open('/Users/richardgan/Pictures/Machine Learning/test_dogsVcats/318.jpg').convert('L')
+model = models.load_model('trained_models/dogsVcats_model3.h5')  # load trained model
+test_img = Image.open('/Users/richardgan/Pictures/Machine Learning/test_dogsVcats/318.jpg').convert('L')  # load image
 test_img = test_img.resize((100, 100))
 test_img = np.asarray(test_img) / 255
 test_img = np.reshape(test_img, (1, 100, 100, 1))
